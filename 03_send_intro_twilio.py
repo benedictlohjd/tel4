f6302d28d9160e3c690fed8a8724a102784e74d2
benedictlohjd <benedictlohjd@gmail.com>
2024-06-22 17:41:19 +0800

    Trigger messages via post api call to execute flow

--- 03_send_intro_twilio.py
+++ 03_send_intro_twilio.py
@@ -20,6 +20,9 @@ from sys import exit
 
 #%%
 
+flow_sid = 'FWcfb4c89a60dba069e779ea82584e4f5f'
+messaging_service = 'MG83cd4bb8925889ff75ad342587513137'
+
 dt_of_log = datetime.now().strftime('%d%m%Y_%H%M%S')
 log_file_name = f"send_intro_{dt_of_log}.log"
 
@@ -31,41 +34,45 @@ logging.basicConfig(
     encoding='utf-8', 
     level=logging.DEBUG
     )
-
+logger.info("Starting program to send intro messages")
+logger.info(f"Using Flow SID: {flow_sid}")
+logger.info(f"Using messaging service: {flow_sid}")
 #%%
+
 
 @sleep_and_retry
 @limits(calls=200, period=timedelta(seconds=60).total_seconds())
-def send_intro_msg(content_sid: str, from_: str, date_contacted: str, to: str) -> Any: # TODO: how to properly annotate this?
+def send_intro_msg(
+    flow_sid: str,
+    msg_service_sid: str, 
+    date_contacted: str, 
+    to: str) -> Any: # TODO: how to properly annotate this?
     
     # Format phone number and format date that participant was recruited to the study
     number = 'whatsapp:+65' + to
     date_contacted_formatted = datetime.strptime(date_contacted, '%d%m%y').strftime("%d/%m/%Y")
 
-    # Send the message
-    message = client.messages.create(
-        content_sid=content_sid, # HXXXXXXXXXXXXXXXXXXXXXXXXXX; WhatsApp content template ID
-        from_= from_,            # MGXXXXXXXXXXXXXXXXXXXXXXXXXXX; messaging service ID
-        content_variables=json.dumps({
+    # Trigger the flow
+    execution = client.studio.v2.flows(flow_sid).executions.create(
+        parameters={
             '1': date_contacted_formatted
-        }),
-        to=number
+            }, 
+        to=number, 
+        from_=msg_service_sid
         )
-    return message
+    logger.info(f"Triggered flow execution {execution.sid} for mobile number {to}")
+
+    return execution
 
 #%%
 
-parent_path = Path(__file__).parent
-env_path = parent_path / ".env"
-data_path = parent_path / "data" / "subjects.csv"
+parent_path = Path(__file__).parent.parent
+env_path = parent_path / "telproject" / ".env"
+data_path = parent_path / "telproject" / "data" / "subjects_test.csv"
 
 logger.info(f"Parent path: {parent_path}")
 logger.info(f"Environment variables at {env_path}")
 
-#%%
-# Set Twilio whatsapp content template ID and Twilio messaging service ID
-content_sid = ''
-messaging_service = ''
 #%%
 # Load twilio tokens
 
@@ -79,6 +86,7 @@ client = Client(account_sid, auth_token)
 if account_sid and auth_token:
     logger.info(f"Twilio auth tokens successfully retrieved")
 else:
+    logging.shutdown()
     exit("twilio authentication tokens not loaded")
 
 #%%
@@ -88,47 +96,51 @@ try:
     logger.info(f"Data of study subjects read in from {data_path}")
 except FileNotFoundError:
     logger.exception("Data of study subjects not found")
+    logging.shutdown()
     exit("FileNotFoundError for data of study subjects")
     
+df_intro.fillna('', inplace=True)
 
 #%%
 
-msg_id = list()
+execution_id = list()
 status = list()
-error_code = list()
 date_updated = list()
+mobile = list()
 
 # Send introduction messages
 for i in tqdm(range(len(df_intro))):
     contact_number, date_contacted = df_intro.loc[i, 'Mobile'], df_intro.loc[i, 'Date']
-    msg = send_intro_msg(
-        content_sid, messaging_service, date_contacted, contact_number
-    )
-    
-    if msg.error_code:
-        logger.info(f"Message {msg.sid} sent to {contact_number} with error code {msg.error_code}")
-    else:
-        logger.info(f"Message {msg.sid} sent to {contact_number}")
+    if contact_number and date_contacted:
+        execution = send_intro_msg(
+            flow_sid, messaging_service, date_contacted, contact_number
+        )
 
-    msg_id.append(msg.sid)
-    status.append(msg.status)
-    error_code.append(msg.error_code)
-    date_updated.append(
-        msg.date_updated.strftime("%d/%m/%Y %H:%M:%S %Z")
-    )
+        execution_id.append(execution.sid)
+        status.append(execution.status)
+        date_updated.append(
+            execution.date_updated.strftime("%d/%m/%Y %H:%M:%S %Z")
+        )
+        mobile.append(contact_number)
+    else:
+        subject_name = df_intro.loc[i, 'Name']
+        logger.info(f"Missing mobile number or date contacted for subject {subject_name}")
 
 #%%
 
 # Create output dataframe 
 out = pd.DataFrame({
-    'message_ID': msg_id,
+    'message_ID': execution_id,
+    'mobile': mobile,
     'status': status,
-    'error': error_code,
     'date_updated': date_updated 
 })
 
-out_filename = "send_intro" + "_" + datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+out_filename = "send_intro" + "_" + str(datetime.now().strftime("%d-%m-%Y_%H%M%S")) + '.csv'
 
-out.to_csv(out_filename)
+out.to_csv(out_filename, sep=",", index=False)
+
+logger.info("Finished program to send intro messages")
 
 logging.shutdown()
+# %%
